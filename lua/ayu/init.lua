f68be80ba114746deff2ea36a475338b6b343779
reanshen <reanshen@tencent.com>
2022-12-06 11:42:22 +0800

    colorcolumn设置为灰色

--- lua/ayu/init.lua
+++ lua/ayu/init.lua
@@ -32,7 +32,7 @@ local function set_groups()
     NormalFloat = { bg = colors.bg },
     FloatBorder = { fg = colors.comment },
     FloatTitle = { fg = colors.fg },
-    ColorColumn = { bg = colors.line },
+    ColorColumn = { bg = colors.comment },
     Cursor = { fg = colors.bg, bg = colors.fg },
     CursorColumn = { bg = colors.line },
     CursorLine = { bg = colors.line },
